b4c507ce6a1a6c0de400048a7721c30fd4e76752
Sweetaxonomy <Sirvent.kaufmann@gmail.com>
2026-07-08 23:24:42 +0200

    fix: use absolute path for SQLite database to fix Codespaces migration

--- src/app.py
+++ src/app.py
@@ -32,7 +32,8 @@ db_url = os.getenv("DATABASE_URL") or None
 if db_url:
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace("postgres://", "postgresql://")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'database.db')}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)

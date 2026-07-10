c93242efe95452c6fa4176874157ad10ea3e3465
Sweetaxonomy <85126277+Sweetaxonomy@users.noreply.github.com>
2026-07-10 17:02:32 +0000

    new migrations

--- render_build.sh
+++ render_build.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
-# exit on error
+
+# Exit on error
 set -o errexit
 
 npm install
 npm run build
 
 pip install pipenv
-
 pipenv install
 
-PYTHONPATH=src pipenv run python -c "
-from app import app
-from api.models import db
-from flask_migrate import stamp
-with app.app_context():
-    db.create_all()
-    stamp(revision='head')
-    print('Tables created and alembic stamped at head')
-"
+PYTHONPATH=src pipenv run flask --app src/app.py db upgrade
